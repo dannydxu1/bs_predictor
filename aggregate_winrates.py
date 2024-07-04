84410cb27e7ab11c35b5120b25031ef681921c8f
Danny Xu <realdannyxu@gmail.com>
2024-07-03 23:31:51 -0700

    added hash function to remove duplicate battles, can get up to 2k battles

--- aggregate_winrates.py
+++ aggregate_winrates.py
@@ -3,6 +3,7 @@ import json
 import os
 from dotenv import load_dotenv
 import time
+import hashlib
 
 # Load environment variables from .env file
 start_time = time.time()
@@ -34,12 +35,22 @@ def update_brawler_stats(brawler_stats, brawler_name, win_status):
         brawler_stats[brawler_name] = {"win": 0, "loss": 0}
     if result == "victory":
         brawler_stats[brawler_name]["win"] += 1
-        # print(brawler_name, "win")
     elif result == "defeat":
         brawler_stats[brawler_name]["loss"] += 1
-        # print(brawler_name, "loss")
 
-def fetch_battle_log_first_pass(player_tag, brawler_stats, seen_players):
+# Create a hash using SHA-256 based off battle time and all six sorted player names
+def create_battle_hash(battle_time, teams):
+    hash_input = battle_time
+    players = []
+    for team in teams:
+        for player in team:
+            players.append(player['name'])
+    players.sort()
+    hash_input += "".join(players)
+    # print(hash_input)
+    return hashlib.sha256(hash_input.encode()).hexdigest()
+
+def fetch_battle_log_first_pass(player_tag, brawler_stats, seen_players, processed_battles):
     BASE_URL = f'https://api.brawlstars.com/v1/players/{player_tag.replace("#", "%23")}/battlelog'
     response = requests.get(BASE_URL, headers=HEADERS)
     if response.status_code == 200:
@@ -52,6 +63,11 @@ def fetch_battle_log_first_pass(player_tag, brawler_stats, seen_players):
             # Ignore solo and duo showdown games
             if battle.get('mode') in ['soloShowdown', 'duoShowdown']:
                 continue
+
+            battle_hash = create_battle_hash(item.get('battleTime'), battle.get('teams', []))
+            processed_battles.add(battle_hash)
+            global battles
+            battles += 1
 
             # Process Winners and Losers
             teams = battle.get('teams', [])
@@ -68,7 +84,8 @@ def fetch_battle_log_first_pass(player_tag, brawler_stats, seen_players):
     else:
         print(f"Failed to fetch battle log: {response.status_code}, {response.text}")
 
-def fetch_battle_log_sequential_passes(player_tag, brawler_stats):
+
+def fetch_battle_log_sequential_passes(player_tag, brawler_stats, processed_battles):
     BASE_URL = f'https://api.brawlstars.com/v1/players/{player_tag.replace("#", "%23")}/battlelog'
     response = requests.get(BASE_URL, headers=HEADERS)
     if response.status_code == 200:
@@ -81,6 +98,16 @@ def fetch_battle_log_sequential_passes(player_tag, brawler_stats):
             # Ignore solo and duo showdown games
             if battle.get('mode') in ['soloShowdown', 'duoShowdown']:
                 continue
+        
+            battle_hash = create_battle_hash(item.get('battleTime'), battle.get('teams', []))
+            if battle_hash in processed_battles:
+                global dupes
+                dupes += 1
+                print("DUPLICATE BATTLE DETECTED, #: ",dupes)
+                continue
+            processed_battles.add(battle_hash)
+            global battles
+            battles += 1
 
             # Process Winners and Losers
             teams = battle.get('teams', [])
@@ -94,20 +121,25 @@ def fetch_battle_log_sequential_passes(player_tag, brawler_stats):
                     update_brawler_stats(brawler_stats, player['brawler']['name'], not primary_team_victory)
     else:
         print(f"Failed to fetch battle log: {response.status_code}, {response.text}")
+
+dupes = 0
+battles = 0
 
 def main():
     seen_players = set()
     brawler_stats = {}
-    fetch_battle_log_first_pass(PLAYER_TAG, brawler_stats, seen_players)
+    processed_battles = set()
+
+    fetch_battle_log_first_pass(PLAYER_TAG, brawler_stats, seen_players, processed_battles)
     print(f'Initial player count: {len(seen_players)}')
     count = 1
     # Fetch battle logs for new player tags
     for new_player_tag in seen_players:
         count += 1
         formatted_percentage = "{:.2f}".format(((count / len(seen_players)) * 100))
-        print(f"Currently {formatted_percentage}% done")
+        print(f"Status: {formatted_percentage}% done")
         if new_player_tag != PLAYER_TAG:
-            fetch_battle_log_sequential_passes(new_player_tag, brawler_stats)
+            fetch_battle_log_sequential_passes(new_player_tag, brawler_stats, processed_battles)
 
     # Calculate win rates
     for brawler, stats in brawler_stats.items():
@@ -125,6 +157,7 @@ def main():
     with open('combined_brawler_winrates.json', 'w') as file:
         file.write(pretty_brawler_stats)
     
+    print(f'Sucessfully evaluted {battles} unique battles.')
     print("Combined brawler winrates saved to 'combined_brawler_winrates.json'")
     end_time = time.time()
     elapsed_time = end_time - start_time
